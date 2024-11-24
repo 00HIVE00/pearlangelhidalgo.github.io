27dab1530ce8fc18c0252008b0f10d6e0f677cc9
00HIVE00 <93169269+00HIVE00@users.noreply.github.com>
2024-11-24 17:21:20 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+  //  Typing Text Animation  //
+
+  var typed = new Typed(".typing", {
+    strings: [
+      "Student",
+      "Virtual Assistant",
+     
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
